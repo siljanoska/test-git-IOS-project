7543c9f2aa0098b6650f0fa87049fd9d5c91d79f
TheMac <mac@TheMacs-MacBook-Pro.local>
2014-12-14 02:04:31 +0100

    add tstclass

---
+++ gitTest/Testclass.h
@@ -0,0 +1,13 @@
+//
+//  Testclass.h
+//  gitTest
+//
+//  Created by TheMac on 12/14/14.
+//  Copyright (c) 2014 TheMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Testclass : UIViewController
+
+@end
